e94ad6a39a4e4400b827445323596ba623314cc7
Christoph Burschka <christoph@burschka.de>
2008-03-28 02:02:41 +0000

    Fixed name, sort links, padding.

--- modules/ranking.php
+++ modules/ranking.php
@@ -4,18 +4,21 @@ function page_ranking($sort, $region, $r = FALSE) {
   $orders = array('given' => 'given', 'received' => 'received', 'name' => 'nation');
   $order = $orders[$sort] .= (!empty($r) == ($sort!='name')) ? ' ASC' : ' DESC';
   $result = db_query('SELECT nation, given, received FROM {nation} WHERE region="%s" ORDER BY '. $order, $region);
-  while ($row = db_fetch_array($result)) $rows[] = $row;
+  while ($row = db_fetch_array($result)) {
+    $row['nation'] = n($row['nation']);
+    $rows[] = $row;
+  }
   $header = array(
     'nation' => array(
-      'data' => l('ranking/name'. ($sort == 'name' && !$r) ? '/r' : '', t('Nation')), 
+      'data' => l('ranking/name'. ($sort == 'name' && !$r ? '/r' : ''), t('Nation')), 
       'class' => ($sort == 'name' ? 'sorted-' . ($r ? 'down' : 'up') : 'sort-up'),
     ),  
     'given' => array(
-      'data' => l('ranking/given'. ($sort == 'given' && !$r) ? '/r' : '', t('Given')),
+      'data' => l('ranking/given'. ($sort == 'given' && !$r ? '/r' : ''), t('Given')),
       'class' => ($sort == 'given' ? 'sorted-' . ($r ? 'up' : 'down') : 'sort-down'),
     ),
     'received' => array(
-      'data' => l('ranking/received'. ($sort == 'received' && !$r) ? '/r' : '', t('Received')),
+      'data' => l('ranking/received'. ($sort == 'received' && !$r ? '/r' : ''), t('Received')),
       'class' => ($sort == 'received' ? 'sorted-' . ($r ? 'up' : 'down') : 'sort-down'),
     ),
   );
