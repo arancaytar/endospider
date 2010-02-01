cca963825cf317ffce14947337e94cfdab41a523
Christoph Burschka <christoph@burschka.de>
2010-02-01 13:13:08 +0000

    Assuming a linear growth of influence based on incoming endos, show the growth share of every nation.

--- modules/ranking.php
+++ modules/ranking.php
@@ -1,16 +1,20 @@
 <?php
 
 function page_ranking($sort, $region, $r = FALSE) {
+  $endos = db_fetch_array(db_query('SELECT COUNT(*) AS saturation FROM {endorsement} WHERE region="%s"', $region));  
   $orders = array('given' => 'given', 'received' => 'received', 'name' => 'nation');
   $order = $orders[$sort] .= (!empty($r) == ($sort!='name')) ? ' ASC' : ' DESC';
   $result = db_query('SELECT nation, given, received, flag, influence FROM {nation} WHERE region="%s" ORDER BY '. $order, $region);
   $i = $j = $last = 0;
+  $cumul = 0;
   while ($row = db_fetch_array($result)) {
     $row['rank'] = ++$i && ($last != $row['received']) ? $i : $j;
     $last = $row['received'];
     $j = $row['rank'];
     $row['nation'] = nl($row['nation']);
     $row['flag'] = flag($row['flag']);
+    $cumul += $row['received'] / $endos * 100;
+    $row['growth'] = '<span title="'. sprintf('%0.2f', $cumul) . '%">'. sprintf('%0.2f', $row['received'] / $endos * 100) .'%</span>';
     $rows[] = $row;
   }
   $header = array(
@@ -31,6 +35,7 @@ function page_ranking($sort, $region, $r = FALSE) {
       'data' => t('Flag'),
     ),
     'influence' => t('Influence'),
+    'growth' => t('Growth share'),
   );
   $page->title = t('Ranking nations in @region', array('@region' => n($region)));
   $page->content = html_table($header, $rows);
